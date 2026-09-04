aa1f96348d5cf7ea6772ecefb7e6d8030942e1ed
Deepak Kiran <deepak@powercen.com>
2026-09-04 16:56:09 +0530

    Debug: log claims and force legal role

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -1,13 +1,8 @@
 module.exports = async function (context, req) {
-  const claims = req.body?.claims || [];
-  const legalGroupId = "4809f241-0a7a-408f-8ce8-e80b62b13753";
-
-  const groupsClaim = claims.find(c => c.typ === "groups");
-  const isLegalMember = groupsClaim && groupsClaim.val.includes(legalGroupId);
-
+  context.log("Claims received:", JSON.stringify(req.body));
   context.res = {
     body: {
-      roles: isLegalMember ? ["legal"] : []
+      roles: ["legal"]
     }
   };
 };
